7c0c8cc8449e272e53c00a7241d6c78a3b526033
zhangjinyu <869554973@qq.com>
2021-04-01 10:22:05 +0800

    htmlhintrc task

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = (grunt) => {
+  grunt.initConfig({
+    htmlhint: {
+  
+      options: {
+        htmlhintrc: './.htmlhintrc'
+      },
+      
+ 
+      
+        
+      src: ['*.html']
+      
+    }
+  });
+ 
+  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.registerTask('default', ['htmlhint']);
+};

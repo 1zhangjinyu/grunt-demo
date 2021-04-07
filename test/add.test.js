e8c91863cb0436fa3ddfcbf485d5bfd10a329462
zhangjinyu <869554973@qq.com>
2021-04-07 15:04:55 +0800

    自动测试

--- test/add.test.js
+++ test/add.test.js
@@ -1,6 +1,6 @@
 const add = require('../add.js'),
       expect = require('chai').expect;
-
+/*
 describe('加法函数的单元测试',()=>{
   it('0 + 0 = 0',()=>{
     expect(add(0,0)).to.be.equal(0);
@@ -10,8 +10,26 @@ describe('加法函数的单元测试',()=>{
     expect(add(3,5)).to.be.equal(8);
   });
 
-  it('a + b = NaN',()=>{
-    expect(isNaN(add('a','b'))).to.be.ok;
+  it('1 + a = NaN',()=>{
+    expect(isNaN(add(1,'a'))).to.be.ok;
   });
 
+});
+*/
+describe('加法函数的动态测试',()=>{
+  let data = [{
+    input:[0,0],
+    output:0
+  },{
+    input:[3,5],
+    output:8
+  },{
+    input:[1,2e3],
+    output:2001
+  }];
+  data.forEach((item)=>{
+    it(`${item.input[0]} + ${item.input[1]} = ${item.output}`,()=>{
+      expect(add(item.input[0],item.input[1])).to.be.equal(item.output);
+    });
+  });
 });
